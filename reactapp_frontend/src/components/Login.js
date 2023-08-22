095fa57a2cf0c29b619d82817aa619e4e4254dda
Duane Francis Rodrigues <74784006+Duane0410@users.noreply.github.com>
2023-08-22 16:33:57 +0530

    Form validation for Registration and login

--- reactapp_frontend/src/components/Login.js
+++ reactapp_frontend/src/components/Login.js
@@ -1,28 +1,68 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
-function Login()
-{
+function Login() {
+
+    const userRef = useRef()
+    const errRef = useRef()
+
+    const [user, setUser] = useState('')
+    const [pass, setPass] = useState('')
+    const [errMsg, setErrMsg] = useState('')
+    const [success, setSuccess] = useState('')
+
+    useEffect(() => {
+        userRef.current.focus()
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('')
+    }, [user, pass])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+
     return(
         <div className="login template d-flex justify-content-center align-items-center 100-w vh-100 bg-primary ">
             <div className="w-40 p-5 rounded bg-white">
-            <form>
-                <h3 className="text-center">Sign In</h3>
-                <div className="mb-2">
-                <label htmlFor="email" >username</label>
-                <input type="email" placeholder=" enter username" className="form-control"></input>
-                </div>
-                <div className="mb-2">
-                <label htmlFor="password" >Password</label>
-                <input type="password" placeholder=" enter password" className="form-control"></input> 
-                </div>
-                <div className="d-grid">
-                       <button className="btn btn-primary">Sign-in</button>
-                </div>
-                <p className="text-end mt-2">
-                    Not registered yet?<Link to='/signup' className="ms-2">Sign Up</Link>
+                <p ref={errRef} className={errMsg ? 'text-danger' : 'd-none'} aria-live='assertive'>
+                    {errMsg}
                 </p>
-            </form>
+                <form onSubmit={handleSubmit}>
+                    <h3 className="text-center">Login</h3>
+                    <div className="mb-2">
+                        <label htmlFor="username" >Username: </label>
+                        <input 
+                            type="text"
+                            placeholder=" Enter username"
+                            className="form-control"
+                            ref={userRef}
+                            autoComplete="off"
+                            onChange={e => setUser(e.target.value)}
+                            required
+                        />
+                    </div>
+
+                    <div className="mb-2">
+                        <label htmlFor="password" >Password: </label>
+                        <input 
+                            type="password" 
+                            placeholder=" Enter password" 
+                            className="form-control"
+                            onChange={e => setPass(e.target.value)}
+                            required
+                        /> 
+                    </div>
+
+                    <div className="d-grid">
+                        <button className="btn btn-primary">Sign In</button>
+                    </div>
+                    <p className="text-end mt-2">
+                        Not registered yet?<Link to='/signup' className="ms-2">Sign Up</Link>
+                    </p>
+                </form>
             </div>
             
         </div>
--- reactapp_frontend/src/components/Register.js
+++ reactapp_frontend/src/components/Register.js
@@ -1,36 +1,192 @@
-import React,{Component} from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { faCheck, faTimes, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from "react-router-dom";
 
-function Register()
-{
-        return(
-            <div className="login template d-flex justify-content-center align-items-center 100-w vh-100 bg-primary ">
+const NAME_REGEX = /^[A-Z][a-z]{3,7}[ ]{0,1}[A-Z]{0,1}[a-z]{0,10}$/
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/
+const PASS_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
+
+function Register() {
+
+    const nameRef = useRef()
+    const errRef = useRef()
+
+    const [name, setName] = useState('')
+    const [validName, setValidName] = useState(false)
+    const [nameFocus, setNameFocus] = useState(false)
+
+    const [user, setUser] = useState('')
+    const [validUserName, setValidUserName] = useState(false)
+    const [userFocus, setUserFocus] = useState(false)
+
+    const [pass, setPass] = useState('')
+    const [validPass, setValidPass] = useState(false)
+    const [passFocus, setPassFocus] = useState(false)
+
+    const [errMsg, setErrMsg] = useState('')
+    const [success, setSuccess] = useState('')
+
+    useEffect(() => {
+        nameRef.current.focus()
+    }, [])
+
+    useEffect(() => {
+        const result = NAME_REGEX.test(name)
+        console.log('Results name - ', result)
+        console.log('name - ', name)
+        setValidName(result)
+    }, [name])
+
+    useEffect(() => {
+        const result = USER_REGEX.test(user)
+        console.log('Results user - ', result)
+        console.log('Username - ', user)
+        setValidUserName(result)
+    }, [user])
+
+    useEffect(() => {
+        const result = PASS_REGEX.test(pass)
+        console.log('Results pass - ', result)
+        console.log('Password - ', pass)
+        setValidPass(result)
+    }, [pass])
+
+    useEffect(() => {
+        setErrMsg('')
+    }, [name, user, pass])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        setSuccess(true)
+    }
+
+    return(
+        <div className="login template d-flex justify-content-center align-items-center 100-w vh-100 bg-primary ">
             <div className="w-40 p-5 rounded bg-white">
-            <form>
-                <h3 className="text-center">Sign Up</h3>
-                <div className="mb-2">
-                <label htmlFor="email" >Name</label>
-                <input type="text" placeholder=" enter name" className="form-control"></input>
-                </div>
-                <div className="mb-2">
-                <label htmlFor="email" >Username</label>
-                <input type="numeric" placeholder=" enter username" className="form-control"></input>
-                </div>
-                <div className="mb-2">
-                <label htmlFor="email" >Password</label>
-                <input type="email" placeholder=" enter password" className="form-control"></input>
-                </div>
-                <div className="d-grid">
-                <Link to='/' className="ms-2"><button className="btn btn-primary">Register</button></Link>
-                </div>
-                <p className="text-end mt-2">
-                    Already registered?<Link to='/' className="ms-2">Sign in</Link>
+                <p ref={errRef} className={errMsg ? 'text-danger' : 'd-none'} aria-live='assertive'>
+                    {errMsg}
                 </p>
-            </form>
+                <form onSubmit={handleSubmit}>
+                    <h3 className="text-center">Register</h3>
+                    <div className="mb-2">
+                        <label htmlFor="name" >
+                            Name: 
+                            <span className={validName ? 'valid text-success' : 'd-none'}>
+                                <FontAwesomeIcon icon={faCheck} />
+                            </span>
+                            <span className={validName || !name ? 'd-none' : 'invalid text-danger'}>
+                                <FontAwesomeIcon icon={faTimes} />
+                            </span>
+                        </label>
+                        <input 
+                            type="text"
+                            placeholder=" Enter name"
+                            className="form-control mb-2"
+                            id='name'
+                            ref={nameRef}
+                            autoComplete='off'
+                            onChange={e => setName(e.target.value)}
+                            required
+                            aria-invalid={validName ? 'false' : 'true'}
+                            aria-describedby='nidnote'
+                            onFocus={() => setNameFocus(true)}
+                            onBlur={() => setNameFocus(false)}
+                        />
+                        <p id='nidnote' style={{fontSize: '0.75rem'}} className={nameFocus && name && !validName ? 'instructions text-danger' : 'd-none'}>
+                            <FontAwesomeIcon icon={faInfoCircle} />
+                            Name can be entered as given name<br />
+                            and surname seperated by a blank space.<br />
+                            Given name can be 4 to 8 characters.<br />
+                            Surname can be 0 to 10 characters.<br />
+                            Must begin with a capital letter.<br />
+                            Numbers, underscores, hyphens are not<br />
+                            allowed.
+                        </p>
+                    </div>
+
+                    <div className="mb-2">
+                        <label htmlFor="username" >
+                            Username: 
+                            <span className={validUserName ? 'valid text-success' : 'd-none'}>
+                                <FontAwesomeIcon icon={faCheck} />
+                            </span>
+                            <span className={validUserName || !user ? 'd-none' : 'invalid text-danger'}>
+                                <FontAwesomeIcon icon={faTimes} />
+                            </span>
+                        </label>
+                        <input 
+                            type="text"
+                            placeholder=" Enter username"
+                            className="form-control mb-2"
+                            id='username'
+                            autoComplete='off'
+                            onChange={e => setUser(e.target.value)}
+                            required
+                            aria-invalid={validUserName ? 'false' : 'true'}
+                            aria-describedby='uidnote'
+                            onFocus={() => setUserFocus(true)}
+                            onBlur={() => setUserFocus(false)}
+                        />
+                        <p id='uidnote' style={{fontSize: '0.75rem'}} className={userFocus && user && !validUserName ? 'instructions text-danger' : 'd-none'}>
+                            <FontAwesomeIcon icon={faInfoCircle} />
+                            4 to 24 characters.<br />
+                            Must begin with a letter.<br />
+                            Letters, numbers, underscores, hyphens<br />
+                            allowed.
+                        </p>
+                    </div>
+
+                    <div className="mb-2">
+                        <label htmlFor="password" >
+                            Password: 
+                            <span className={validPass ? 'valid text-success' : 'd-none'}>
+                                <FontAwesomeIcon icon={faCheck} />
+                            </span>
+                            <span className={validPass || !pass ? 'd-none' : 'invalid text-danger'}>
+                                <FontAwesomeIcon icon={faTimes} />
+                            </span>
+                        </label>
+                        <input
+                            type="password"
+                            placeholder=" Enter password"
+                            className="form-control mb-2"
+                            id='password'
+                            onChange={e => setPass(e.target.value)}
+                            required
+                            aria-invalid={validPass ? 'false' : 'true'}
+                            aria-describedby='passnote'
+                            onFocus={() => setPassFocus(true)}
+                            onBlur={() => setPassFocus(false)}
+                        />
+                        <p id='passnote' style={{fontSize: '0.75rem'}} className={passFocus && !validPass ? 'instructions text-danger' : 'd-none'}>
+                            <FontAwesomeIcon icon={faInfoCircle} />
+                            8 to 24 characters.<br />
+                            Must include uppercase and lowercase<br />
+                            letters, a number and a special character.<br />
+                            Allowed special characters:
+                            <span aria-label='exclamation mark'>!</span>
+                            <span aria-label='at symbol'>@</span>
+                            <span aria-label='hashtag'>#</span>
+                            <span aria-label='dollar sign'>$</span>
+                            <span aria-label='percent'>%</span>
+                        </p>
+                    </div>
+
+                    <div className="d-grid">
+                        <button disabled={!validName || !validUserName || !validPass ? true : false} className="btn btn-primary">
+                            Sign up
+                        </button>
+                    </div>
+                    <p className="text-end mt-2">
+                        Already registered?<Link to='/' className="ms-2">Sign in</Link>
+                    </p>
+                </form>
             </div>
             
         </div>
-        )
+    )
  }
 
  export default Register
