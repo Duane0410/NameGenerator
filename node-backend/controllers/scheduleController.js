3a5f43d43f6e53b11cd4a5e85181856c6244f3eb
Duane Francis Rodrigues <74784006+Duane0410@users.noreply.github.com>
2023-09-15 13:56:56 +0530

    Update schedule backend completed

--- node-backend/controllers/scheduleController.js
+++ node-backend/controllers/scheduleController.js
@@ -1,7 +1,27 @@
 const User = require('../model/User');
 
 const handleSchedule = async (req, res) => {
+    const {user, weekly, monthly, quarterly, yearly, updated_only} = req.body
     
+    if (!user) {
+        return res.status(400).json({ "message": 'Username is required!' });
+    }
+
+    const foundUser = await User.findOne({ username: user }).exec();
+    if (!foundUser) {
+        return res.status(204).json({ "message": `No username ${user}.` });
+    }
+    console.log('Found User - ', foundUser)
+
+    foundUser.weekly = weekly
+    foundUser.monthly = monthly
+    foundUser.quarterly = quarterly
+    foundUser.yearly = yearly
+    foundUser.updated_only = updated_only
+
+    const result = await foundUser.save();
+    console.log('Result - ', result)
+    res.status(201).json({ 'success': `User ${user} schedule updated!` });
 }
 
 module.exports = { handleSchedule };
