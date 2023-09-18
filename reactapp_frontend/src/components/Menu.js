00d1268badaefd92993470a5e78c082190247fa3
Duane Francis Rodrigues <74784006+Duane0410@users.noreply.github.com>
2023-09-18 18:16:24 +0530

    Submenu fixes

--- reactapp_frontend/src/components/Menu.js
+++ reactapp_frontend/src/components/Menu.js
@@ -2,129 +2,163 @@ import React, { useEffect, useState } from 'react';
 import useLogout from '../hooks/useLogout';
 import { useNavigate, Outlet, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { faRightFromBracket, faAngleDown } from '@fortawesome/free-solid-svg-icons';
 import '../static/menu.css';
+import useAxiosPrivate from '../hooks/useAxiosPrivate';
 
 const Menu = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const logout = useLogout();
-  const currentUrl = location.pathname;
+    const navigate = useNavigate();
+    const location = useLocation();
+    const logout = useLogout();
+    const currentUrl = location.pathname;
+    const fullUrl = window.location.href
+    const axiosPrivate = useAxiosPrivate()
 
-  const [hasSubtitle, setHasSubtitle] = useState(false);
-  const [fullUrl, setFullUrl] = useState(window.location.href);
-  const [subTitle, setSubTitle] = useState('');
-  const [isHomeDropdownOpen, setIsHomeDropdownOpen] = useState(false);
+    const [isHomeDropdownOpen, setIsHomeDropdownOpen] = useState(false);
+    const [data, setData] = useState()
 
-  const homeDropdownItems = [
-    { label: 'Option 1', path: '/option1' },
-    { label: 'Option 2', path: '/option2' },
-    // Add more submenu items for "Home" as needed
-  ];
+    const [homeDropdownItems, setHomeDropdownItems] = useState([
+        { label: 'Option 1', path: '/teams' },
+        { label: 'Option 2', path: '/team-controll' },
+    ])
 
-  const handleClick = (address) => {
-    if (address === 'resources') {
-        const searchParams = new URLSearchParams(location.search)
-        const resource = searchParams.get('resource')
-        const categories = searchParams.get('categories')
-        navigate(`/resources?resource=${encodeURIComponent(resource)}&categories=${encodeURIComponent(categories)}`)
-    } else if (address.includes('http')) {
-        const urlObject = new URL(address)
-        const destination = urlObject.pathname + urlObject.search
-        navigate(`${destination}`)
-    } else {
-        navigate(`${address}`)
+    const handleClick = (address) => {
+        navigate(`${address}`);
+        if (address === '/' || address === '/set-schedule') {
+            setIsHomeDropdownOpen(false)
+            return
+        }
+        window.location.reload();
+
+    };
+
+    const getQueryParameter = (url, parameterName) => {
+        const queryParams = new URLSearchParams(new URL(url).search);
+        return queryParams.get(parameterName);
     }
-    setIsHomeDropdownOpen(false);
-    // ... (unchanged)
-  };
 
-  const signOut = async () => {
-    await logout();
-    navigate('/login');
-  };
+    const signOut = async () => {
+        await logout();
+        navigate('/login');
+    };
 
-  useEffect(() => {
-    // ... (unchanged)
-  }, [currentUrl]);
+    useEffect(() => {
 
-  const toggleHomeDropdown = () => {
-    setIsHomeDropdownOpen(!isHomeDropdownOpen);
-  };
+        let isMounted = true
+        const controller = new AbortController()
 
-  return (
-    <div className='App nav-space'>
-      <div class="l-navbar" id="nav-bar">
-        <nav class="nav">
-          <div>
-            <div class="nav_logo">
-              <i class='bx bx-layer nav_logo-icon' /> 
-              <span class="nav_logo-name">
-               InfraNameGen
-              </span>
+        const getData = async () => {
+            try {
+                const response = await axiosPrivate.get(`/resource-types`, {
+                    signal: controller.signal
+                })
+                console.log(response.data)
+                isMounted && setData(response.data)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        getData()
+
+
+        return () => {
+            isMounted = false
+            controller.abort()
+        }
+    }, [])
+
+    useEffect(() => {
+        const arrayDrop = []
+        if (data) {
+            data.map(item => {
+                arrayDrop.push({ label: `${item.resource_type}`, path: `/resources?resource=${encodeURIComponent(item.resource_type)}&categories=${encodeURIComponent(item.name_categories)}`})
+                if (getQueryParameter(fullUrl, 'resource') === item.resource_type) setIsHomeDropdownOpen(true)
+            })
+        }
+        setHomeDropdownItems(arrayDrop)
+
+        
+    }, [data])
+
+    useEffect(() => {
+        const linkColor = document.querySelectorAll('.nav_link');
+        console.log('Current - ', currentUrl)
+        console.log('Full Current - ', fullUrl)
+
+        const removeColor = () => {
+            if (linkColor) {
+                linkColor.forEach((e) => e.classList.remove('active'));
+            }
+        };
+
+        removeColor();
+
+        if (currentUrl) {
+            const putColor = document.getElementById(currentUrl);
+            if (putColor?.classList) putColor.classList.add('active');
+        }
+    }, [currentUrl]);
+
+    const toggleHomeDropdown = () => {
+        setIsHomeDropdownOpen(!isHomeDropdownOpen);
+    };
+
+    return (
+        <div className='App nav-space'>
+            <div className="l-navbar" id="nav-bar">
+                <nav className="nav">
+                    <div>
+                        <a onClick={() => handleClick('/')} className="nav_logo nav_link">
+                            <i className='bx bx-layer nav_logo-icon' /> 
+                            <span className="nav_logo-name">
+                                InfraNameGen
+                            </span>
+                        </a>
+
+                        <div className="nav_list">
+                            <div className="nav_link" onClick={toggleHomeDropdown}>
+                                <i className={`bx bx-grid-alt nav_icon ${isHomeDropdownOpen ? 'rotate-icon' : ''}`} />
+                                <span className={`nav_name ${(isHomeDropdownOpen) ? 'font-weight-bold' : ''}`}>
+                                    Resources <FontAwesomeIcon icon={faAngleDown} />
+                                </span>
+                            </div>
+
+                            {isHomeDropdownOpen && (
+                                <div className="submenu">
+                                    {homeDropdownItems.map((item, index) => (
+                                        <a
+                                            key={index}
+                                            onClick={() => handleClick(item.path)}
+                                            className={`nav_link ${(item.path === currentUrl || getQueryParameter(fullUrl, 'resource') === item.label) ? 'active' : ''}`}
+                                        >
+                                            <span className="nav_name px-4">
+                                                {item.label}
+                                            </span>
+                                        </a>
+                                    ))}
+                                </div>
+                            )}
+                        </div>
+
+                        <a onClick={() => handleClick('/set-schedule')} id='/set-schedule' className="nav_link">
+                            <i className='bx bx-user nav_icon' />
+                            <span className="nav_name">
+                                Email Schedule 
+                            </span>
+                        </a>
+                    </div>
+                    <a className="nav_link btn btn-info text-light mx-4" onClick={signOut}>
+                        <span><FontAwesomeIcon icon={faRightFromBracket} /> Sign Out</span>
+                    </a>
+                </nav>
             </div>
-            <div class="nav_list">
-              <div className="nav_item">
-              <a onClick={() => handleClick('/')} id='/' className="nav_link">
-                <i className='bx bx-user nav_icon' />
-                <span className="nav_name">
-                  Home
-                </span>
-              </a>
-                <div className="nav_link" onClick={toggleHomeDropdown}>
-                  <i className={`bx bx-grid-alt nav_icon ${isHomeDropdownOpen ? 'rotate-icon' : ''}`} />
-                  <span className="nav_name">
-                   Resources
-                  </span>
-                </div>
-                {isHomeDropdownOpen && (
-                  <div className="submenu">
-                    {homeDropdownItems.map((item, index) => (
-                      <a key={index} onClick={() => handleClick(item.path)} className="nav_link">
-                        <i className='bx bx-user nav_icon' />
-                        <span className="nav_name">
-                          {item.label}
-                        </span>
-                      </a>
-                    ))}
-                  </div>
-                )}
-              </div>
-              {hasSubtitle && (
-                subTitle === 'Update' ? (
-                  <a href={fullUrl} id='subtitle' className="nav_link">
-                    <i className='bx bx-grid-alt nav_icon' />
-                    <span className="nav_name">
-                      {subTitle}
-                    </span>
-                  </a>
-                ) : (
-                  <a onClick={() => handleClick(fullUrl)} id='subtitle' className="nav_link">
-                    <i className='bx bx-grid-alt nav_icon' />
-                    <span className="nav_name">
-                      {subTitle}
-                    </span>
-                  </a>
-                )
-              )}
-              <a onClick={() => handleClick('/set-schedule')} id='/set-schedule' className="nav_link">
-                <i className='bx bx-user nav_icon' />
-                <span className="nav_name">
-                  Email Schedule 
-                </span>
-              </a>
+
+            <div className='content'>
+                <Outlet />
             </div>
-          </div>
-          <a className="nav_link btn btn-info text-light mx-4" onClick={signOut}>
-            <span><FontAwesomeIcon icon={faRightFromBracket} /> Sign Out</span>
-          </a>
-        </nav>
-      </div>
-      <div className='content'>
-        <Outlet />
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default Menu;
